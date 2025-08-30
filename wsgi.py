c38dc10500fc55c785318398af190f3a86523a84
Tanner Bowman <tannerbowman@gmail.com>
2025-08-30 17:03:36 -0600

    Add wsgi.py for gunicorn compatibility

---
+++ wsgi.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""
+WSGI entry point for gunicorn
+"""
+import os
+import sys
+
+# Add backend to path
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'backend'))
+
+# Import the Flask app
+from app import app
+
+# This is what gunicorn will use
+application = app
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
